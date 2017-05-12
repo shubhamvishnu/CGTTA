52ba9a81c1c911d2f036386bf22da7196c251af4
shubhamvishnu <shubhamvishnu@gmail.com>
2017-05-13 01:55:05 +0530

    Able to retrieve data.

--- app/src/main/java/com/cgtta/cgtta/RankingListActivity.java
+++ app/src/main/java/com/cgtta/cgtta/RankingListActivity.java
@@ -16,17 +16,23 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class RankingListActivity extends AppCompatActivity {
     TableLayout rankingListTable;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
-    Map<Integer, DataSnapshot> yearListSnapShot;
-    int selectedYear = 2017;
+    List<String> colHeadersList;
+    String selectedYear = "2017";
     String playerCategory = "Cadet_Girls";
+    List<String> rowContentList;
+    List<List<String>> rowsList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,55 +48,52 @@ public class RankingListActivity extends AppCompatActivity {
         databaseReference.keepSynced(true);
 
         rankingListTable = (TableLayout) findViewById(R.id.ranking_list_table_layout);
-        yearListSnapShot = new HashMap<>();
+
+        colHeadersList = new ArrayList<>();
+        rowContentList = new ArrayList<>();
+        rowsList = new ArrayList<>();
         initLists();
 
     }
 
     void initLists() {
-        databaseReference.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                yearListSnapShot.put(Integer.parseInt(dataSnapshot.getKey()), dataSnapshot.child(dataSnapshot.getKey()));
-                showContent();
-            }
+      databaseReference.child(selectedYear+"/"+playerCategory).addListenerForSingleValueEvent(new ValueEventListener() {
+          @Override
+          public void onDataChange(DataSnapshot dataSnapshot) {
+              String title = dataSnapshot.child("title").getValue().toString();
 
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+              for (DataSnapshot snapshot : dataSnapshot.child("headers").getChildren()) {
+                  colHeadersList.add(snapshot.getValue().toString());
+              }
 
-            }
+              for(DataSnapshot snapshot : dataSnapshot.child("rowContents").getChildren()){
+                for(DataSnapshot snapshotChild : snapshot.getChildren()){
+                    rowContentList.add(snapshotChild.getValue().toString());
+                }
+                rowsList.add(rowContentList);
+              }
+              Toast.makeText(RankingListActivity.this, rowsList.toString(), Toast.LENGTH_SHORT).show();
 
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
 
-            }
+          }
 
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+          @Override
+          public void onCancelled(DatabaseError databaseError) {
 
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+          }
+      });
     }
-
+/*
     void showContent() {
-
-        DataSnapshot datasnapshot = yearListSnapShot.get(selectedYear).child(playerCategory);
-
-
-        String title = datasnapshot.child("title").getValue().toString();
-        int noOfHeadersCount = (int) datasnapshot.child("headers").getChildrenCount();
-        DataSnapshot headerSnapshot = datasnapshot.child("headers");
-        TableRow headerRow = new TableRow(this);
+        String title = dataSnapshot.child("title").getValue().toString();
+        int noOfHeadersCount = (int) dataSnapshot.child("headers").getChildrenCount();
+        DataSnapshot headerSnapshot = dataSnapshot.child("headers");
+        TableRow headerRow = new TableRow(RankingListActivity.this);
         headerRow.setLayoutParams(new TableRow.LayoutParams(
                 TableRow.LayoutParams.FILL_PARENT,
                 TableRow.LayoutParams.WRAP_CONTENT));
         for (int i = 0; i < noOfHeadersCount; i++) {
-            TextView headerRowValue = new TextView(this);
+            TextView headerRowValue = new TextView(RankingListActivity.this);
             headerRowValue.setText(headerSnapshot.child("" + i).getValue().toString());
             headerRowValue.setTextColor(Color.BLACK);
             headerRowValue.setTypeface(Typeface.DEFAULT, Typeface.BOLD);
@@ -101,6 +104,9 @@ public class RankingListActivity extends AppCompatActivity {
         rankingListTable.addView(headerRow);
 
 
+
+
     }
+    */
 
 }
